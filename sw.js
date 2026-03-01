94d92c5aa71fb18006d4df1ecc7a1fc10090bb2d
comunicasindico <auditorambientalperito@gmail.com>
2026-03-01 16:01:06 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,32 @@
-const CACHE_NAME="harmonia-cache-v1";
+const CACHE_NAME="harmonia-cache-v2";
 const urlsToCache=["./","./index.html"];
 
 self.addEventListener("install",event=>{
 event.waitUntil(
-caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache))
+caches.open(CACHE_NAME).then(cache=>{
+return cache.addAll(urlsToCache);
+})
+);
+});
+
+self.addEventListener("activate",event=>{
+event.waitUntil(
+caches.keys().then(cacheNames=>{
+return Promise.all(
+cacheNames.map(cache=>{
+if(cache!==CACHE_NAME){
+return caches.delete(cache);
+}
+})
+);
+})
 );
 });
 
 self.addEventListener("fetch",event=>{
 event.respondWith(
-caches.match(event.request).then(response=>{
-return response||fetch(event.request);
+fetch(event.request).catch(()=>{
+return caches.match(event.request);
 })
 );
 });
