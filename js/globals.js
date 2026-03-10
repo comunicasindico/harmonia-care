3fc508c4da8ac2d1da8c204c487003f495db141f
comunicasindico <auditorambientalperito@gmail.com>
2026-03-10 15:27:09 -0300

    Update globals.js

--- js/globals.js
+++ js/globals.js
@@ -1,8 +1,8 @@
 /* ====================================================
 000 – VARIÁVEIS GLOBAIS DO SISTEMA
 ==================================================== */
-
 let TURNO_ATUAL="manha"
 let PACIENTE_ATUAL=null
 let db=null
 let EMPRESA_ID="d9f678e5-6c7a-485e-895c-cb4791db840e"
+let ROTINAS_CACHE=[]
