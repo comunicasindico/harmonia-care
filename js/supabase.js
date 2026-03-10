e846adbae34ba4d84f445d7a306a3161b970f528
comunicasindico <auditorambientalperito@gmail.com>
2026-03-10 15:51:40 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -3,7 +3,7 @@
 ==================================================== */
 const SUPABASE_URL="https://whvwqektkinnhdprehss.supabase.co"
 const SUPABASE_ANON_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndodndxZWt0a2lubmhkcHJlaHNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIyOTY2MzYsImV4cCI6MjA4Nzg3MjYzNn0.gdTMT25dc4x7YlLQEWHKd-6dM32nKp5mnRwMk_fiEdU"
-const supabaseClient = window.supabase.createClient(
+const supabaseClient = supabase.createClient(
 SUPABASE_URL,
 SUPABASE_ANON_KEY
 )
