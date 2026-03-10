c13f363f739c139a0e080bedb5675312c20e05f4
comunicasindico <auditorambientalperito@gmail.com>
2026-03-10 00:28:39 -0300

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,1 +1,68 @@
 
+/* ====================================================
+CARREGAR PROFISSIONAIS
+==================================================== */
+
+async function carregarProfissionais(){
+
+const {data}=await db
+.from("profissionais")
+.select("*")
+.order("nome")
+
+let html=""
+
+data.forEach(p=>{
+
+html+=`
+
+<div class="drag-item"
+draggable="true"
+ondragstart="drag(event)"
+id="prof_${p.id}">
+
+${p.nome}
+
+</div>
+
+`
+
+})
+
+document.getElementById("listaProfissionaisDrag").innerHTML=html
+
+}
+
+/* ====================================================
+CARREGAR PACIENTES DRAG
+==================================================== */
+
+async function carregarPacientesDrag(){
+
+const {data}=await db
+.from("pacientes")
+.select("*")
+.order("nome_completo")
+
+let html=""
+
+data.forEach(p=>{
+
+html+=`
+
+<div class="drag-item"
+ondrop="drop(event)"
+ondragover="allowDrop(event)"
+id="pac_${p.id}">
+
+${p.nome_completo}
+
+</div>
+
+`
+
+})
+
+document.getElementById("listaPacientesDrag").innerHTML=html
+
+}
