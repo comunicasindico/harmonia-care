0044263f0825cbe15e7dcc947beafffe53ed5bfe
comunicasindico <auditorambientalperito@gmail.com>
2026-03-10 00:28:58 -0300

    Update pdf.js

--- js/pdf.js
+++ js/pdf.js
@@ -1,1 +1,30 @@
 
+/* ====================================================
+GERAR PDF GERAL
+==================================================== */
+
+async function gerarPDFGeral(){
+
+const { jsPDF } = window.jspdf
+const doc = new jsPDF()
+
+doc.text("Relatório Geral",20,20)
+
+doc.save("relatorio.pdf")
+
+}
+
+/* ====================================================
+GERAR PDF PACIENTE
+==================================================== */
+
+async function gerarPDFPaciente(){
+
+const { jsPDF } = window.jspdf
+const doc = new jsPDF()
+
+doc.text("Relatório Paciente",20,20)
+
+doc.save("paciente.pdf")
+
+}
