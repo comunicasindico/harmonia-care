3038a4c5d15afc98bdb07aef0564ef71de3092be
comunicasindico <auditorambientalperito@gmail.com>
2026-03-10 15:05:24 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,18 +1,16 @@
 /* ====================================================
 001 – CONFIG SUPABASE
 ==================================================== */
-const SUPABASE_URL = "https://whvwqektkinnhdprehss.supabase.co"
-const SUPABASE_KEY ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndodndxZWt0a2lubmhkcHJlaHNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIyOTY2MzYsImV4cCI6MjA4Nzg3MjYzNn0.gdTMT25dc4x7YlLQEWHKd-6dM32nKp5mnRwMk_fiEdU"
-const supabaseClient=window.supabase.createClient(
+const SUPABASE_URL="https://whvwqektkinnhdprehss.supabase.co"
+const SUPABASE_ANON_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndodndxZWt0a2lubmhkcHJlaHNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIyOTY2MzYsImV4cCI6MjA4Nzg3MjYzNn0.gdTMT25dc4x7YlLQEWHKd-6dM32nKp5mnRwMk_fiEdU"
+const supabaseClient = window.supabase.createClient(
 SUPABASE_URL,
 SUPABASE_ANON_KEY
 )
 db = supabaseClient
-const EMPRESA_ID = "d9f678e5-6c7a-485e-895c-cb4791db840e"
 /* ====================================================
 002 – VARIÁVEIS GLOBAIS
 ==================================================== */
-
 let TURNO_ATUAL = "manha"
 let ROTINAS_CACHE = []
 
