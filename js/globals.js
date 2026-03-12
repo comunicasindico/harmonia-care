7847fe1f44c7b05b2a0ebb2e045ee99fccfdba05
comunicasindico <auditorambientalperito@gmail.com>
2026-03-12 13:34:45 -0300

    Update globals.js

--- js/globals.js
+++ js/globals.js
@@ -1,18 +1,30 @@
 /* ====================================================
 000 – VARIÁVEIS GLOBAIS DO SISTEMA
 ==================================================== */
-let TURNO_ATUAL = "manha"
+if(typeof TURNO_ATUAL==="undefined"){
+var TURNO_ATUAL="manha"
+}
 
-let PACIENTE_ATUAL = null
+if(typeof PACIENTE_ATUAL==="undefined"){
+var PACIENTE_ATUAL=null
+}
 
-let db = null
+if(typeof db==="undefined"){
+var db=null
+}
 
-/* ID da empresa no Supabase */
-const EMPRESA_ID = "d9f678e5-6c7a-485e-895c-cb4791db840e"
+if(typeof EMPRESA_ID==="undefined"){
+var EMPRESA_ID="d9f678e5-6c7a-485e-895c-cb4791db840e"
+}
 
-let ROTINAS_CACHE = []
+if(typeof ROTINAS_CACHE==="undefined"){
+var ROTINAS_CACHE=[]
+}
 
-let ROTINAS_GERADAS = false
+if(typeof ROTINAS_GERADAS==="undefined"){
+var ROTINAS_GERADAS=false
+}
 
-/* modo edição painel clínico */
-let MODO_EDICAO_CLINICO = false
+if(typeof MODO_EDICAO_CLINICO==="undefined"){
+var MODO_EDICAO_CLINICO=false
+}
