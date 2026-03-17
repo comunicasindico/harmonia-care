9738a2f174a6e6761ed2265b4576b6b68e413f7c
comunicasindico <auditorambientalperito@gmail.com>
2026-03-16 21:45:35 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-let salvandoPendencias=false
+window.salvandoPendencias=false
 /* ====================================================
 010 – LOGIN
 ==================================================== */
