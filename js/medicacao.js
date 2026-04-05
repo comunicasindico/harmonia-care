bfc0bead2215db494f71a7c7ab3ef64ae248aadf
comunicasindico <auditorambientalperito@gmail.com>
2026-04-04 22:56:02 -0300

    Update medicacao.js

--- js/medicacao.js
+++ js/medicacao.js
@@ -242,7 +242,7 @@ return (txt||"")
 .toLowerCase()
 .normalize("NFD")
 .replace(/[\u0300-\u036f]/g,"")
-.replace(/\s+/g," ").trim
+.replace(/\s+/g," ").trim()
 .replace(/mg|cp|cps|ml|ui/g,"")
 .trim()
 }
